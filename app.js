040b650b9d5bd577e697e98201aa03d570fc4676
Bertrand31 <bertrandjun@gmail.com>
2016-01-31 21:23:50 +0100

    YA TOU KI MARCHE

--- app.js
+++ app.js
@@ -18,15 +18,19 @@ app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
 // TEMPLATES
-var page = jade.compileFile('./templates/page.jade');
+var page    = jade.compileFile('./templates/page.jade'),
+    content = jade.compileFile('./templates/content.jade');
 
 // ROUTES
 // Static files
 app.use('/', express.static(__dirname + '/library/built', { maxAge: 86400 }));
 // Homepage
-app.get('/', function(req, res) { res.render('home'); });
+app.get('/', function(req, res) {
+    var pageContent = generator.getJSON('home', function(JSON) {
+        res.send(page(JSON));
+    });
+});
 // Pages
-var newPage = jade.compileFile('./templates/page.jade');
 app.get('/:name', function(req, res) {
     var pageContent = generator.getJSON(req.params.name, function(JSON) {
         res.send(page(JSON));
@@ -36,8 +40,8 @@ app.get('/:name', function(req, res) {
 io.sockets.on('connection', function(socket) {
 
     socket.on('getContent', function(target) {
-        var pageContent = generator.getJSON(target, function(JSON) {
-            socket.emit('content', page(JSON));
+        var subContent = generator.getJSON(target, function(JSON) {
+            socket.emit('content', content(JSON));
         });
     });
 
